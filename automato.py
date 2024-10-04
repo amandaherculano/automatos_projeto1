360e8c9cbd59ae97fd9269d9a09aae21bc79afd5
Amanda <22.00986-8@maua.br>
2024-10-04 09:22:18 -0300

    comentarios

--- automato.py
+++ automato.py
@@ -1,16 +1,17 @@
 from tkinter import *
 
-class VendingMachineAutomaton:
+class AutomatoMaquinaVendas:
     def __init__(automato):
-        # Estados e estado inicial
-        automato.states = {
+        # Estados
+        automato.estados = {
             's0': 0.00, 's1': 0.25, 's2': 0.50, 's3': 0.75,
             's4': 1.00, 's5': 1.25, 's6': 1.50, 's7': 1.75, 's8': 2.00
         }
-        automato.current_state = 's0'
+        # inicia com estado inicial, saldo = 0
+        automato.estado_atual = 's0'
 
         # Função de transição
-        automato.transitions = {
+        automato.transicoes = {
             ('s0', 'm25'): ('s1', 'n'), ('s0', 'm50'): ('s2', 'n'), ('s0', 'm100'): ('s4', 'n'),
             ('s1', 'm25'): ('s2', 'n'), ('s1', 'm50'): ('s3', 'n'), ('s1', 'm100'): ('s5', 'n'),
             ('s2', 'm25'): ('s3', 'n'), ('s2', 'm50'): ('s4', 'n'), ('s2', 'm100'): ('s6', 'n'),
@@ -20,18 +21,19 @@ class VendingMachineAutomaton:
             ('s6', 'm25'): ('s7', 'n'), ('s6', 'm50'): ('s8', 'n'), ('s6', 'm100'): ('s8', 't50'),
             ('s7', 'm25'): ('s8', 'n'), ('s7', 'm50'): ('s8', 't25'), ('s7', 'm100'): ('s8', 't75'),
             ('s8', 'm25'): ('s8', 't25'), ('s8', 'm50'): ('s8', 't50'), ('s8', 'm100'): ('s8', 't100'),
-            ('s8', 'b'): ('s0', 'r'),  # Liberar refrigerante
+            # Ao acionar botao, deve ter 2 reias no saldo, ou nao ira retornar nada 'n'
+            ('s8', 'b'): ('s0', 'r'),  # Libera refrigerante
             ('s0', 'b'): ('s0', 'n'), ('s1', 'b'): ('s1', 'n'), ('s2', 'b'): ('s2', 'n'),
             ('s3', 'b'): ('s3', 'n'), ('s4', 'b'): ('s4', 'n'), ('s5', 'b'): ('s5', 'n'),
             ('s6', 'b'): ('s6', 'n'), ('s7', 'b'): ('s7', 'n')
         }
 
-    def input(automato, coin_or_button):
-        """Processa uma entrada (moeda ou botão) e altera o estado."""
-        if (automato.current_state, coin_or_button) in automato.transitions:
-            next_state, output = automato.transitions[(automato.current_state, coin_or_button)]
-            automato.current_state = next_state
-            return output
+    # Processa uma entrada (moeda ou botão) e altera o estado.
+    def entrada(automato, moeda_ou_botao):
+        if (automato.estado_atual, moeda_ou_botao) in automato.transicoes:
+            proximo_estado, saida = automato.transicoes[(automato.estado_atual, moeda_ou_botao)]
+            automato.estado_atual = proximo_estado
+            return saida
         else:
             return 'n'  # Nada acontece
 
@@ -42,7 +44,7 @@ janela.title("Simulador de Máquina de Vendas")
 janela.geometry("700x300")
 
 # Instanciar o autômato
-vm = VendingMachineAutomaton()
+vm = AutomatoMaquinaVendas()
 
 texto_titulo = Label(janela, text="Máquina de Refrigerante", font=("Calibri", 15), fg="darkolivegreen")
 texto_titulo.grid(row=0, column=0, pady=20, padx=10)
@@ -63,15 +65,15 @@ def alterar_saldo(coin_value):
     global saldo
     # Limpa a mensagem quando uma moeda é inserida
     texto_liberacao["text"] = ""
-    output = vm.input(coin_value)
-    saldo = vm.states[vm.current_state]
+    saida = vm.entrada(coin_value)
+    saldo = vm.estados[vm.estado_atual]
     texto_saldo["text"] = "Saldo: R$ " + str(saldo)
-    if output.startswith('t'):
-        if output == 't25':
+    if saida.startswith('t'):
+        if saida == 't25':
             texto_liberacao["text"] = "Troco: R$ 0.25"
-        elif output == 't50':
+        elif saida == 't50':
             texto_liberacao["text"] = "Troco: R$ 0.50"
-        elif output == 't100':
+        elif saida == 't100':
             texto_liberacao["text"] = "Troco: R$ 1.00"
 
 # Botões de moeda
@@ -81,8 +83,8 @@ botao_25.grid(row=2, column=1, padx=10, pady=10)
 botao_50 = Button(janela, text="R$ 0.50", command=lambda: alterar_saldo('m50'), bg="ivory3", fg="cornsilk1", width=10, height=1, font=("Calibri", 12))
 botao_50.grid(row=2, column=2, padx=10, pady=10)
 
-botao_100 = Button(janela, text="R$ 1.00", command=lambda: alterar_saldo('m100'), bg="gold2", fg="cornsilk1", width=10, height=1 , font=("Calibri", 12))
-botao_100.grid(row=2, column=3, padx=10, pady=10)
+botao_liberar_refri00 = Button(janela, text="R$ 1.00", command=lambda: alterar_saldo('m100'), bg="gold2", fg="cornsilk1", width=10, height=1 , font=("Calibri", 12))
+botao_liberar_refri00.grid(row=2, column=3, padx=10, pady=10)
 
 # Função para liberar refrigerante
 def liberar_refrigerante():
@@ -94,8 +96,8 @@ def liberar_refrigerante():
         texto_liberacao["text"] = "Saldo insuficiente! Insira mais moedas."
 
 # Botão para liberar refrigerante
-botao_1 = Button(janela, text="Liberar Refrigerante", command=liberar_refrigerante,  font=("Calibri", 14), bg='darkolivegreen', fg='cornsilk')
-botao_1.grid(row=3, columnspan=5, pady=30)
+botao_liberar_refri = Button(janela, text="Liberar Refrigerante", command=liberar_refrigerante,  font=("Calibri", 14), bg='darkolivegreen', fg='cornsilk')
+botao_liberar_refri.grid(row=3, columnspan=5, pady=30)
 
 # Exibir mensagem de liberação
 texto_liberacao = Label(janela, text="", fg="darkolivegreen", font=("Calibri", 13))
